690032a46d44e4f5a52328f7d7c62485d68df9cc
samepant <sampanter@gmail.com>
2020-11-21 10:03:14 -0600

    ejected create-react-app to get styled-jsx working

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,29 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
       </header>
+      <style jsx global>
+      {`
+        body {
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+            'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+            sans-serif;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+        }
+
+        code {
+          font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+            monospace;
+        }
+      `}
+      </style>
     </div>
   );
 }
